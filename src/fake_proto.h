3454439ccc9d1400f62934fdfaefae75b0929105
Marcin Junczys-Dowmunt <marcinjd@microsoft.com>
2018-11-29 21:16:32 -0800

    more attempts at repmving proto

--- src/fake_proto.h
+++ src/fake_proto.h
@@ -5,6 +5,15 @@
 
 namespace fake_proto {
 
+class Message {
+public:
+  template <class T> 
+  add(std::string key, const& T value) {
+    
+  }
+
+}
+
 template <class T>
 class Vector {
 private:
@@ -13,6 +22,7 @@ private:
 public:
   T* Add() { data_.emplace_back(); return &data_.back(); }
   T* Mutable(int index) { return &data_[index]; }
+  const T& At(int index) const { return data_[index]; }
   void SwapElements(int i, int j) { std::swap(data_[i], data_[j]); }
 
   size_t size() const { return data_.size(); }
@@ -32,6 +42,7 @@ public: \
   const ::fake_proto::Vector<type>& variable() const { return variable ## _; } \
   type* add_ ## variable() { return variable ## _.Add(); } \
   type* mutable_ ## variable(int index) { return variable ## _.Mutable(index); } \
+  const type& variable(int index) const { return variable ## _.At(index); } \
   ::fake_proto::Vector<type>* mutable_ ## variable() { return & variable ## _ ; } \
   int variable ## _size() const { return variable ## _.size(); } \
 
@@ -44,21 +55,54 @@ public: \
   void set_ ## variable(std::string&& value) { variable ## _ = value; } \
   void set_ ## variable(const char* value) { variable ## _ = value; } \
   void set_ ## variable(const char* value, size_t size) { variable ## _ = std::string(value, size); }; \
-  std::string* mutable_ ## variable() { return & variable ## _ ; }
+  std::string* mutable_ ## variable() { return & variable ## _ ; } \
+  bool has_ ## variable() const { return true; }
 
-#define PB_UINT32(variable) \
+#define PB_STRING_DEFAULT(variable, dvalue) \
 private: \
-  uint32_t variable ## _; \
+  std::string variable ## _{dvalue}; \
+public: \
+  const std::string& variable() const { return variable ## _; } \
+  void set_ ## variable(const std::string& value) { variable ## _ = value; } \
+  void set_ ## variable(std::string&& value) { variable ## _ = value; } \
+  void set_ ## variable(const char* value) { variable ## _ = value; } \
+  void set_ ## variable(const char* value, size_t size) { variable ## _ = std::string(value, size); }; \
+  std::string* mutable_ ## variable() { return & variable ## _ ; } \
+  bool has_ ## variable() const { return true; }
+
+#define PB_UINT32(variable, dvalue) \
+private: \
+  uint32_t variable ## _{dvalue}; \
 public: \
   const uint32_t& variable() const { return variable ## _; } \
   void set_ ## variable(uint32_t value) { variable ## _ = value; }
 
-#define PB_FLOAT(variable) \
+#define PB_INT32(variable, dvalue) \
 private: \
-  float variable ## _; \
+  int32_t variable ## _{dvalue}; \
+public: \
+  const int32_t& variable() const { return variable ## _; } \
+  void set_ ## variable(int32_t value) { variable ## _ = value; }
+
+#define PB_FLOAT(variable, dvalue) \
+private: \
+  float variable ## _{dvalue}; \
 public: \
   const float& variable() const { return variable ## _; } \
   void set_ ## variable(float value) { variable ## _ = value; }
 
+#define PB_BOOL(variable, dvalue) \
+private: \
+  bool variable ## _{dvalue}; \
+public: \
+  const bool& variable() const { return variable ## _; } \
+  void set_ ## variable(bool value) { variable ## _ = value; }
+
+#define PB_MEMBER(type, variable) \
+private: \
+  type variable ## _; \
+public: \
+  const type& variable() const { return variable ## _; } \
+  type* mutable_ ## variable() { return &variable ## _; }
 
 }
--- src/sentencepiece.pb.h
+++ src/sentencepiece.pb.h
@@ -7,18 +7,18 @@ namespace sentencepiece {
 struct SentencePieceText {
   struct SentencePiece {
     PB_STRING(piece)
-    PB_UINT32(id)
+    PB_UINT32(id, 0)
 
     PB_STRING(surface)
-    PB_UINT32(begin)
-    PB_UINT32(end)
+    PB_UINT32(begin, 0)
+    PB_UINT32(end, 0)
 
     void Clear() { piece_.clear(); id_ = 0; surface_.clear(); begin_ = 0; end_ = 0; }
   };
 
   PB_REPEAT(SentencePiece, pieces)
   PB_STRING(text)
-  PB_FLOAT(score)
+  PB_FLOAT(score, 0.f)
 
   void Clear() { text_.clear(); pieces_.clear(); score_ = 0; }
   std::string Utf8DebugString() { return text(); }
